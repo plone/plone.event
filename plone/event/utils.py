235fee951787142c48115c7126b190f4002ace89
Johannes Raggam <raggam-nl@adm.at>
2010-10-17 22:31:05 +0200

    more utils from P.DRI + doctest unittests

--- plone/event/utils.py
+++ plone/event/utils.py
@@ -5,6 +5,8 @@
 __author__ = """Jens Klein <jens@bluedynamics.com>,
                 Johannes Raggam <johannes@raggam.co.at>"""
 
+import pytz
+from datetime import datetime
 from datetime import timedelta
 
 DSTADJUST = 'adjust'
@@ -42,3 +44,92 @@ def utcoffset_normalize(date, delta=None, dstmode=DSTAUTO):
         return date.replace(tzinfo=date.tzinfo.normalize(date).tzinfo)
     else: # DSTKEEP
         return date.tzinfo.normalize(date)
+
+
+def utctz():
+    return pytz.timezone('UTC')
+
+
+def utc(dt):
+    """convert python datetime to UTC."""
+    if dt is None:
+        return None
+    return dt.astimezone(utctz())
+
+
+# TODO: let guesstz guess the time zone not via zope's DateTime
+def guesstz(DT):
+    """'Guess' pytz from a zope DateTime.
+
+    !!! theres no real good method to guess the timezone.
+    DateTime was build somewhere in 1998 long before python had a working
+    datetime implementation available and still stucks with this incomplete
+    implementation.
+
+    """
+    if DT.timezoneNaive():
+        return utctz()
+    tzname = DT.timezone()
+
+    #    # DateTime timezones not fully compatible with pytz
+    #    # see http://pypi.python.org/pypi/DateTime/2.12.0
+    #    if tzname.startswith('GMT'):
+    #        tzname = 'Etc/%s' % tzname
+    try:
+        tz = pytz.timezone(tzname)
+        return tz
+    except KeyError:
+        pass
+    return None
+
+
+def pydt(dt):
+    """converts a zope DateTime in a python datetime.
+    """
+    if dt is None:
+        return None
+
+    if isinstance(dt, datetime):
+        return utcoffset_normalize(dt, dstmode=DSTADJUST)
+
+    tz = guesstz(dt)
+    if tz is None:
+        dt = dt.toZone('UTC')
+        tz = utctz
+
+    year, month, day, hour, min, sec = dt.parts()[:6]
+    # seconds (parts[6]) is a float, so we map to int
+    sec = int(sec)
+    dt = datetime(year, month, day, hour, min, sec, tzinfo=tz)
+    dt = dt.tzinfo.normalize(dt)
+    return dt
+
+
+def dt2int(dt):
+    """Calculates an integer from a datetime.
+    Resolution is one minute, always relative to utc
+
+    """
+    if dt is None:
+        return 0
+    # TODO: if dt has not timezone information, guess and set it
+    dt = utc(dt)
+    value = (((dt.year*12+dt.month)*31+dt.day)*24+dt.hour)*60+dt.minute
+    return value
+
+
+def int2dt(dtint):
+    """Returns a datetime object from an integer representation with
+    resolution of one minute, relative to utc.
+
+    """
+    if not isinstance(dtint, int):
+        raise ValueError, 'int2dt expects integer values as arguments.'
+    minutes = dtint % 60
+    hours = dtint / 60 % 24
+    days = dtint / 60 / 24 % 31
+    months = dtint / 60 / 24 / 31 % 12
+    years = dtint / 60 / 24 / 31 / 12
+    return datetime(years, months, days, hours, minutes, tzinfo=pytz.timezone('UTC'))
+
+
