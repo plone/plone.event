6608947893ce3f30e6ca2b62513a924864e05a48
Johannes Raggam <raggam-nl@adm.at>
2010-10-21 19:08:09 +0200

    tests for recurrence support

--- plone/event/tests.py
+++ plone/event/tests.py
@@ -5,13 +5,43 @@
 
 import unittest
 import doctest
+import zope.component
 from interlude import interact
 
 OPTIONFLAGS = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 DOCFILES = [
     'recurrence.txt',
+    'recurrence_support.txt',
     'utils.txt',
 ]
+
+from zope.interface import implements
+from plone.event.interfaces import IRecurringEventICal
+class RecurrenceStub(object):
+    """Basic stub object for testing events.
+    """
+    implements(IRecurringEventICal)
+    def __init__(self, recurrence=None, start_date=None, end_date=None):
+        self.recurrence = recurrence
+        self.start_date = start_date
+        self.end_date = end_date
+        self.duration = end_date - start_date
+        #subject
+        #location
+        #wholeDay
+        #startDate
+        #endDate
+        #text
+        #attendees
+        #eventUrl
+        #contactName
+        #contactEmail
+        #contactPhone
+        #recurrence
+        #start_date
+        #end_date
+        #duration
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
@@ -19,6 +49,7 @@ def test_suite():
             docfile,
             optionflags=OPTIONFLAGS,
             globs={'interact': interact,},
+            tearDown=zope.component.testing.tearDown
         ) for docfile in DOCFILES
     ])
     return suite
