835248d3393201ab3a724663d8aef2f37ad26279
Tom Gross <tom@toms-projekte.de>
2010-10-30 21:02:55 +0200

    don't use interact in repository version

--- plone/event/tests/test_doctest.py
+++ plone/event/tests/test_doctest.py
@@ -7,7 +7,7 @@ import os.path
 import unittest
 import doctest
 from zope.component.testing import tearDown
-from interlude import interact
+#from interlude import interact
 
 OPTIONFLAGS = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 DOCFILES = [
@@ -49,7 +49,7 @@ def test_suite():
             os.path.join(os.path.dirname(__file__), '..', docfile),
             module_relative=False,
             optionflags=OPTIONFLAGS,
-            globs={'interact': interact,
+            globs={#'interact': interact,
                    'MockEvent': MockEvent},
             tearDown=tearDown
         ) for docfile in DOCFILES
