d9d055a6a172db65dbffa5f452c9e14a63808ded
Johannes Raggam <raggam-nl@adm.at>
2012-05-28 21:36:58 +0200

    typo

--- plone/event/interfaces.py
+++ plone/event/interfaces.py
@@ -76,8 +76,8 @@ class IRecurrenceSupport(Interface):
     """
 
     def occurrences(self, range_start, range_end):
-        """ Return a list of IEventAccessor wrapper objects with custom
-            attributes of the specific occurrence changed.
+        """ Return a list of IOccurrence objects with custom attributes of the
+            specific occurrence set.
 
             @param range_start: Search for occurrences after this date.
                                 Python datetime.
