3c7d50492746805a88c1711d7fa08c2102078b28
Johannes Raggam <raggam-nl@adm.at>
2013-04-05 16:48:05 +0200

    more tests, raise coverage

--- plone/event/tests/test_adapters.py
+++ plone/event/tests/test_adapters.py
@@ -27,7 +27,30 @@ class TestAdapters(unittest.TestCase):
         obj.start = datetime(2012,12,12,10,0,tzinfo=tz)
         obj.end = datetime(2012,12,12,12,0,tzinfo=tz)
         zope.interface.alsoProvides(obj, IEvent)
+        
+        # Create accessor
         acc = IEventAccessor(obj)
-        self.assertTrue(acc.start == obj.start)
-        self.assertTrue(acc.end == obj.end)
-        self.assertTrue(acc.duration == obj.end - obj.start)
+
+        # Accessor getters
+        self.assertEquals(acc.start, obj.start)
+        self.assertEquals(acc.end, obj.end)
+        self.assertEquals(acc.duration, obj.end - obj.start)
+
+        # Accessor setters
+        start = datetime(2013, 4, 5, 16, 31, tzinfo=tz)
+        end = datetime(2013, 4, 5, 16, 35, tzinfo=tz)
+        acc.start = start
+        acc.end = end
+        self.assertTrue(acc.start == obj.start == start)
+        self.assertTrue(acc.end == obj.end == end)
+
+        # Accessor deletor
+        acc.something = True
+        self.assertTrue(acc.something == obj.something == True)
+        del acc.something
+        self.assertTrue(hasattr(acc, 'something') == False)
+        self.assertTrue(hasattr(obj, 'something') == False)
+
+        del acc.start
+        self.assertTrue(hasattr(acc, 'start') == False)
+        self.assertTrue(hasattr(obj, 'start') == False)
