625796b97bfc18e0da66f34ba4c96dc5c8bde467
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:26:23 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords='Plone calendar calendaring event recurring',
     author='Plone Foundation',
