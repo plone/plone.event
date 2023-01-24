29c6b8a297f08089cfe7de3f56c6885313f3128e
Maurits van Rees <maurits@vanrees.org>
2023-01-24 19:35:24 +0100

    Update classifiers and set python_requires.
    [ci skip]

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
@@ -25,6 +25,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords='Plone calendar calendaring event recurring',
     author='Plone Foundation',
@@ -35,6 +38,7 @@ setup(
     namespace_packages=['plone'],
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     install_requires=[
         'setuptools',
         'python-dateutil',  # >4.0.2
